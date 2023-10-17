8744c9f1702b254ce6028713b540b6df6bdbf373
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-10-17 21:41:55 +0630

    useRoute

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,9 +1,16 @@
 <script setup>
+import { useRoute } from "vue-router";
+import cars from '../data/cars.json'
 
+const route = useRoute();
+
+// useRoute for get route information
+const car = cars.find(car => car.id === parseInt(route.params.id))
 </script>
 
 <template>
     <div>
-        <h1>Car View</h1>
+        <h1>{{ car.name }}</h1>
+        <p>{{ car.price }}</p>
     </div>
 </template>
