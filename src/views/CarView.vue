606d60c74efdf0cd5dd5e75a903ab290d6999046
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-10-19 07:56:52 +0630

    useRouter

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -3,16 +3,25 @@ import { useRoute, useRouter } from "vue-router";
 import cars from '../data/cars.json'
 
 const route = useRoute();
-
+const router = useRouter();
 // useRoute for get route information
-const car = cars.find(car => car.id === parseInt(route.params.id))
+// useRoute is for get current route information
+// useRouter is for modifying route
+
+const carId = parseInt(route.params.id)
+const car = cars.find(car => car.id === carId)
+
+const showContact = () => {
+    if(carId === 4) return;
+    router.push(`/cars/${carId}/contact`)
+}
 </script>
 
 <template>
     <div>
         <h1>{{ car.name }}</h1>
         <p>{{ car.price }}</p>
-        <button>Contact</button>
+        <button @click.prevent="showContact">Contact</button>
 
         <RouterView />
     </div>
